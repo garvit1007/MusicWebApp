42da984027a68e5b59bbd219701e32f84f8714e0
garvit1007 <97176509+garvit1007@users.noreply.github.com>
2023-09-11 18:35:22 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -4,6 +4,10 @@ LeftWristX = 0;
 RightWristX = 0;
 LeftWristY = 0;
 RightWristY = 0;
+scoreLeftWrist = 0;
+StatusOfSong_1 = "";
+scoreRightWrist = 0;
+StatusOfSong_2 = "";
 
 function preload() {
     song = loadSound("music.mp3");
@@ -25,8 +29,38 @@ function modelLoaded() {
     console.log("PoseNet is initialized");
 }
 
+function play() {
+    song.play();
+    song.setVolume(1);
+    song.rate(1);
+}
+
 function draw() {
     image(video, 0, 0, 550, 500);
+
+    StatusOfSong_1 = song.isPlaying();
+    fill("red");
+    stroke("red");
+    if(scoreLeftWrist > 0.2) {
+        circle(LeftWristX, LeftWristY, 20);
+        song_1.stop();
+        if(StatusOfSong_1 == false) {
+            song.play();
+            document.getElementById("song_name").innerHTML = "Song Name is Song 1";
+        }
+    }
+
+    StatusOfSong_2 = song_1.isPlaying();
+    fill("red");
+    stroke("red");
+    if(scoreRightWrist > 0.2) {
+        circle(RightWristX, RightWristY, 20);
+        song.stop()
+        if(StatusOfSong_2 == false) {
+            song_1.play();
+            document.getElementById("song_name").innerHTML = "Song Name is Song 2";
+        }
+    }
 }
 
 function gotPoses(results) {
@@ -36,8 +70,14 @@ function gotPoses(results) {
         LeftWristY = results[0].pose.leftWrist.y;
         console.log("Left Wrist X = " + LeftWristX + "Left Wrsit Y = " + LeftWristY );
 
+        scoreLeftWrist = results[0].pose.keypoints[9].score;
+        console.log("Score of Left wrist = " + scoreLeftWrist);
+
         RighttWristX = results[0].pose.rightWrist.x;
         RightWristY = results[0].pose.rightWrist.y;
         console.log("Right Wrist X = " + RightWristX + "Right Wrsit Y = " + RightWristY );
+
+        scoreRightWrist = results[0].pose.keypoints[10].score;
+        console.log("Score of Right wrist = " + scoreRightWrist);
     }
 }
